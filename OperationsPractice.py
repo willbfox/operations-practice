460e1e9d3d4b35509bccbb50e56f6428783ec1a1
Matt Ellis <43010091+willbfox@users.noreply.github.com>
2018-09-25 12:48:35 -0400

    Add files via upload

---
+++ OperationsPractice.py
@@ -0,0 +1,20 @@
+'''
+Created on Sep 25, 2018
+
+@author: wfox
+Operations ppractice
+'''
+
+print('welcome to the test score calculator')
+print('\n\n\n\n')
+
+#input the test scores
+test1= float(input('Please enter the first test score: '))
+test2= float(input('Please enter the second test score: '))
+test3= float(input('Please enter the third test score: '))
+
+#calculate the average
+average= (test1 + test2 + test3) / 3
+
+#display the output
+print("Average Test Score:", average)
